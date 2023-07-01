c070e42c5c655013619f00494ae1aea686947112
Shravang007 <shravangoudgajula@gmail.com>
2023-07-01 23:34:05 +0530

    vpc networking

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 resource "aws_vpc" "main" {
   cidr_block         = var.cidr_block
   enable_dns_support = true
+  tags = merge({
+    Name = "${var.env}-vpc"}, var.tags)
 }
-#  tags = merge({
-#    Name = "${var.env}-vpc"
-#  },
-#    var.tags)
-#}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
