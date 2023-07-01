daa81b7d439ccc4cbddd81e6a496a64ac8516ed7
Shravang007 <shravangoudgajula@gmail.com>
2023-07-01 23:02:44 +0530

    tags

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+  cidr_block         = var.cidr_block
+  enable_dns_support = true
+  tags = merge({
+    Name = "${var.env}-vpc"
+  },
+    var.tags)
 }
